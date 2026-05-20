49b05c421df3d195eef1a14e23e0b1c0eedba8b9
fominifamily-group <mindset.greg@gmail.com>
2026-05-20 03:42:23 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Family Website Loaded Successfully");
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+});
